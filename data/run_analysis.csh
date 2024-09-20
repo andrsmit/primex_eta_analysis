5b6c383a6703e43cb1ca0479a46e5506e43c6c18
andrsmit <andrsmit@jlab.org>
2024-09-19 20:58:04 -0400

    use bash for job submission instead of csh

--- data/run_analysis.csh
+++ data/run_analysis.csh
@@ -3,12 +3,12 @@
 
 # command line inputs:
 
-set outdir      = $1
-set skim_type   = $2
-set target_type = $3
-set runnumber   = $4
-set phase       = $5
-set jfile       = $6
+set skim_type = $1
+set runnumber = $2
+set tree_file = $3
+set root_file = $4
+set  cfg_file = $5
+set jsub_file = $6
 
 # source GlueX setup scripts:
 
@@ -20,38 +20,43 @@ setenv JANA_CALIB_URL mysql://ccdb_user@hallddb-farm.jlab.org/ccdb
 
 setenv HALLD_MY /home/andrsmit/halld_my
 
+echo "pwd"
+pwd
+
 echo "ls -a"
 ls -a
 
 # copy JANA config file:
 
-echo "cp /work/halld/home/andrsmit/primex_eta_analysis/data/config/eta_ana.conf jana.conf"
-cp /work/halld/home/andrsmit/primex_eta_analysis/data/config/eta_ana.conf jana.conf
-
-# remove symlink created by swif and replace with local file:
-
-#if ( -f /cache$mss_fname ) then
-#	echo "copying file from cache..."
-#	rm $loc_fname
-#	cp /cache$mss_fname $loc_fname
-#endif
+echo "cp ${cfg_file} jana.config"
+cp ${cfg_file} jana.config
 
 # set up and run hd_root:
 
 set bin = ${HALLD_RECON_HOME}/Linux_Alma9-x86_64-gcc11.4.1/bin/hd_root
 
-echo "$bin --config=jana.conf -o ${runnumber}.root ${skim_type}_${runnumber}_*.evio"
-$bin --config=jana.conf -o ${runnumber}.root ${skim_type}_${runnumber}_*.evio
+echo "$bin --config=jana.config ${skim_type}_${runnumber}_*.evio"
+$bin --config=jana.config ${skim_type}_${runnumber}_*.evio
+
+# move output root tree to appropriate directory:
+if ( -f eta_gg.root ) then
+	echo "mv eta_gg.root ${tree_file}"
+	mv eta_gg.root ${tree_file}
+endif
 
 # move output root file to appropriate directory:
-
-echo "mv ${runnumber}.root $outdir/rootFiles/phase${phase}/${target_type}/${runnumber}.root"
-mv ${runnumber}.root $outdir/rootFiles/phase${phase}/${target_type}/${runnumber}.root
+if ( -f hd_root.root ) then
+	echo "mv hd_root.root ${root_file}"
+	mv hd_root.root ${root_file}
+endif
 
 # delete local copies of input files and jsub file:
+
+echo "rm -f jana.config"
+rm -f jana.config
 
 echo "rm -f ${skim_type}_${runnumber}_*.evio"
 rm -f ${skim_type}_${runnumber}_*.evio
 
-echo "rm -f $jfile"
-rm -f $jfile
+echo "rm -f ${jsub_file}"
+rm -f ${jsub_file}
---
+++ data/submit_tree.sh
@@ -0,0 +1,157 @@
+#!/usr/bin/sh
+#
+pass=1
+
+# set which run period you want to analyze:
+phase=$1
+
+#directory where the output will be stored:
+outdir=/work/halld/home/andrsmit/primex_eta_analysis/data
+
+# script that will execute the commands for each job:
+script=${outdir}/run_analysis.csh
+
+# set this to "go" to actually submit the jobs:
+submit_runs=go
+
+# directory where skim files are stored on tape:
+mss_base_dir=/mss/halld/home/gxproj4/skim/phase${phase}
+
+# this variable will just count how many jobs are submitted:
+ij=0
+
+#=========================#
+#Job Resources:
+
+workflow=primex_eta2g-skim_analysis
+account=halld
+partition=production
+ram=12GB
+cores=5
+run_time=2hr
+disk=18GB
+constraint=el9
+
+#=========================#
+
+# look at the 2-photon skims only:
+skim="eta2g-skim"
+
+declare -a target_list=("full" "empty")
+declare -a field_list=("bfield" "nobfield")
+
+# directory where runs lists are stored:
+run_list_dir=/work/halld/home/andrsmit/run_lists/primex_phase${phase}
+
+#=========================#
+
+# make sure config file exists:
+cfg_file=${outdir}/pass${pass}/eta_ana.config
+if [ ! -f $cfg_file ]; then
+	echo "JANA config file does not exist."
+	exit 1
+fi
+
+for target in "${target_list[@]}"
+do
+	for field in "${field_list[@]}"
+	do
+		echo "$target target, $field runs:"
+		
+		# get run list:
+		run_list=${run_list_dir}/he_${target}_${field}.txt
+		
+		# skip this configuration if run list doesn't exist:
+		if [ ! -f $run_list ]; then
+			continue
+		fi
+		
+		# set up output directory where rootTrees will be written:
+		dir_tree=${outdir}/pass${pass}/rootTrees/phase${phase}
+		mkdir -p $dir_tree
+		
+		# set up output directory where hd_root files will be written:
+		dir_rfile=${outdir}/pass${pass}/rootFiles/phase${phase}
+		mkdir -p $dir_rfile
+		
+		# set up output directory where jsub will be written:
+		dir_jsub=${outdir}/pass${pass}/jsub/phase${phase}
+		mkdir -p $dir_jsub
+		
+		# loop over all runs in the list:
+		while read run; do
+			
+			# Convert run number into 6 digit variable:
+			
+			run_number=$run
+			if [[ $run_number -lt "100000" ]]; then
+				run_number="0$run"
+			fi
+			
+			# set up output root tree file name:
+			tree_file=${dir_tree}/${run_number}.root
+			if [ -f $tree_file ]; then
+				continue
+			fi
+			
+			# set up output hd_root file name:
+			root_file=${dir_rfile}/${run_number}.root
+			if [ -f $root_file ]; then
+				continue
+			fi
+			
+			# set up job submission file name:
+			jsub_file=${dir_jsub}/${run_number}.jsub
+			if [ -f $jsub_file ]; then
+				continue
+			fi
+			
+			dir_mss="${mss_base_dir}/ver01/phase${phase}-${field}-primex-eta-${target}/${skim}"
+			
+			#
+			# For phases 1 and 2, all skim files were stored in "ver01".
+			# However, for phase 3, most skim files are in 'ver00', while some runs had to be 
+			# reprocessed and stored in 'ver01'.
+			# To automate the procedure, I first check if the directory for a given run exists in 'ver01'.
+			# If so, I use that version. If not, I check if it exists in 'ver00' and use that one instead.
+			#
+			
+			if [ $phase -eq 3 ]; then
+				dir_mss="${mss_base_dir}/ver01/phase${phase}-primex-eta-${target}/$skim"
+				if [ ! -d ${dir_mss}/${run_number} ]; then
+					dir_mss="${mss_base_dir}/ver00/phase${phase}-primex-eta-${target}/$skim"
+					if [ ! -d ${dir_mss}/${run_number} ]; then
+						echo "Skim files do not exist for Run${run_number}."
+						continue
+					fi
+				fi
+			fi
+			
+			echo "Submitting job for Run${run_number}"
+			
+			# set job name:
+			job_name="${skim}_${run_number}_pass${pass}"
+			
+			command="swif2 add-job -workflow ${workflow}"
+			command="$command -name ${job_name} -account ${account} -partition ${partition}"
+			command="$command -cores ${cores} -ram ${ram} -time ${run_time} -disk ${disk}"
+			command="$command -constraint ${constraint}"
+			for file in ${dir_mss}/Run${run_number}/hd_rawdata_${run_number}_*.evio; do
+				command="$command -input `basename $file` mss:${file}"
+			done
+			command="$command $script $skim $run_number $tree_file $root_file $cfg_file $jsub_file"
+			
+			if [ $submit_runs == "go" ]; then
+				echo "$command" > $jsub_file
+				$command
+			fi
+			ij=$(($ij+1))
+			
+		done < $run_list # end loop over run numbers
+	done # end loop over bfield configs
+done # end loop over target configs
+
+echo "submitted $ij jobs"
+if [ $submit_runs == "go" ]; then
+	swif2 run $workflow
+fi
